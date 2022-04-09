906e4554eca1f3bcf2290ea5b2fdbf5156edf17b
Stella Margonar <margonar@spaziodati.eu>
2022-04-09 18:08:06 +0200

    start react app

--- calcio_splash/settings.py
+++ calcio_splash/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dg2^gx3vuc+v*_krhz*c7%r7+ru#f-ijin^m!4^vo^svpl274n'
+SECRET_KEY = "dg2^gx3vuc+v*_krhz*c7%r7+ru#f-ijin^m!4^vo^svpl274n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,56 +32,57 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'calcio_splash.apps.CalcioSplashConfig',
-    'storages',
-    'dbbackup',
-    'raven.contrib.django.raven_compat',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "calcio_splash.apps.CalcioSplashConfig",
+    "storages",
+    "dbbackup",
+    "raven.contrib.django.raven_compat",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "livematch",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'calcio_splash.urls'
+ROOT_URLCONF = "calcio_splash.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'calcio_splash.wsgi.application'
+WSGI_APPLICATION = "calcio_splash.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -91,16 +92,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -108,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'it-IT'
+LANGUAGE_CODE = "it-IT"
 
-TIME_ZONE = 'Europe/Rome'
+TIME_ZONE = "Europe/Rome"
 
 USE_I18N = True
 
@@ -118,19 +119,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
-DBBACKUP_STORAGE_OPTIONS = {'location': '/var/backups'}
+DBBACKUP_STORAGE = "django.core.files.storage.FileSystemStorage"
+DBBACKUP_STORAGE_OPTIONS = {"location": "/var/backups"}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-local_settings = os.environ.get('DJANGO_EXTRA_SETTINGS')
+local_settings = os.environ.get("DJANGO_EXTRA_SETTINGS")
 if local_settings:
     import importlib
 
     local_settings_folder = os.path.dirname(local_settings)
-    local_settings_filename = os.path.basename(local_settings).replace('.py', '')
+    local_settings_filename = os.path.basename(local_settings).replace(".py", "")
     sys.path.insert(0, local_settings_folder)
     globals().update(importlib.import_module(local_settings_filename).__dict__)
--- calcio_splash/urls.py
+++ calcio_splash/urls.py
@@ -11,26 +11,52 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^regulation/$', TemplateView.as_view(template_name='regulation.html'), name='regulation'),
-    url(r'^fuoriposto/$', TemplateView.as_view(template_name='fuoriposto.html'), name='fuoriposto'),
-    url(r'^iscrizioni/$', TemplateView.as_view(template_name='iscrizioni.html'), name='iscrizioni'),
-    url(r'^raise_exception/$', views.exception),
-
-    url(r'^admin/', admin_site.urls),
-    url(r'^teams/$', views.TeamListView.as_view(), name='teams'),
-    url(r'^team/(?P<pk>\d+)$', views.TeamDetailView.as_view(), name='team-detail'),
-    url(r'^match/(?P<pk>\d+)$', views.MatchDetailView.as_view(), name='match-detail'),
-    url(r'^beachmatch/(?P<pk>\d+)$', views.BeachMatchDetailView.as_view(), name='beach-match-detail'),
-
-    url(r'^group/(?P<pk>\d+)$', views.GroupDetailView.as_view(), name='group-detail'),
-    url(r'^tournament/(?P<pk>\d+)$', views.TournamentDetailView.as_view(), name='tournament-detail'),
-    url(r'^tournament/(?P<pk>\d+)/group/(?P<pk_group>\d+)/matches/$', views.MatchListView.as_view(), name='group_matches'),
-    url(r'^matches/(?P<year>\d+)/$', views.MatchListView.as_view(), name='matches'),
-
-    url(r'^classifiche/(?P<year>\d+)/$', views.AlboView.as_view(), name='albo'),
-    url(r'^classifiche/(?P<gender>\w+)/$', views.AlboMarcatori.as_view(), name='albomarcatori'),
+    url(r"^$", TemplateView.as_view(template_name="index.html"), name="home"),
+    url(
+        r"^regulation/$",
+        TemplateView.as_view(template_name="regulation.html"),
+        name="regulation",
+    ),
+    url(
+        r"^fuoriposto/$",
+        TemplateView.as_view(template_name="fuoriposto.html"),
+        name="fuoriposto",
+    ),
+    url(
+        r"^iscrizioni/$",
+        TemplateView.as_view(template_name="iscrizioni.html"),
+        name="iscrizioni",
+    ),
+    url(r"^raise_exception/$", views.exception),
+    url(r"^admin/", admin_site.urls),
+    url(r"^teams/$", views.TeamListView.as_view(), name="teams"),
+    url(r"^team/(?P<pk>\d+)$", views.TeamDetailView.as_view(), name="team-detail"),
+    url(r"^match/(?P<pk>\d+)$", views.MatchDetailView.as_view(), name="match-detail"),
+    url(
+        r"^beachmatch/(?P<pk>\d+)$",
+        views.BeachMatchDetailView.as_view(),
+        name="beach-match-detail",
+    ),
+    url(r"^group/(?P<pk>\d+)$", views.GroupDetailView.as_view(), name="group-detail"),
+    url(
+        r"^tournament/(?P<pk>\d+)$",
+        views.TournamentDetailView.as_view(),
+        name="tournament-detail",
+    ),
+    url(
+        r"^tournament/(?P<pk>\d+)/group/(?P<pk_group>\d+)/matches/$",
+        views.MatchListView.as_view(),
+        name="group_matches",
+    ),
+    url(r"^matches/(?P<year>\d+)/$", views.MatchListView.as_view(), name="matches"),
+    url(r"^classifiche/(?P<year>\d+)/$", views.AlboView.as_view(), name="albo"),
+    url(
+        r"^classifiche/(?P<gender>\w+)/$",
+        views.AlboMarcatori.as_view(),
+        name="albomarcatori",
+    ),
+    url(r"", include("livematch.urls")),
 ]
 
-handler404 = 'calcio_splash.views.handler404'
-handler500 = 'calcio_splash.views.handler500'
+handler404 = "calcio_splash.views.handler404"
+handler500 = "calcio_splash.views.handler500"
