700433a588376da531df4676afe6c8c448857d3f
Stefano Parmesan <parmesan@spaziodati.eu>
2023-06-21 19:42:03 +0200

    add whitenoise, remove old code

--- calcio_splash/settings.py
+++ calcio_splash/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,12 @@ DBBACKUP_STORAGE_OPTIONS = {"location": "/var/backups"}
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 SENTRY_DSN = None  # set in DJANGO_EXTRA_SETTINGS file
 
