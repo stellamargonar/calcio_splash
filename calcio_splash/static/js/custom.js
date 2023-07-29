135e942c3ba7475b62cc42f9481b7097c613ce39
Stefano Parmesan <parmesan@spaziodati.eu>
2023-07-29 12:34:00 +0200

    fix js errors in console

--- calcio_splash/static/js/custom.js
+++ calcio_splash/static/js/custom.js
@@ -21,10 +21,13 @@
 	$(function() {
 		$('.navbar-nav li a').bind('click', function(event) {
 			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
+			var href = $anchor.attr('href')
+			if (href !== '#' && href.startsWith('#')) {
+				$('html, body').stop().animate({
+					scrollTop: $(href).offset().top
+				}, 1500, 'easeInOutExpo');
+				event.preventDefault();
+			}
 		});
 		$('.page-scroll a').bind('click', function(event) {
 			var $anchor = $(this);
